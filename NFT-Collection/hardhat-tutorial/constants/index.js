af7d8cf277005858b52ad1c3e29d13b1ee61feb9
Edmon Bosch <edmonbosch@fundacionord.cat>
2023-03-02 18:00:58 +0100

    whitelisted contract changed for goerli, and NFT-Collection smart contract released

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x3e47bf680369ecc94Df5492071b5A060ef29Ca89";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
