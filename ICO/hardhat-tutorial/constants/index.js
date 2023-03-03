af2a32f5e64861d0ff684247c1f2251aff3c6c5d
Edmon Bosch <edmonbosch@fundacionord.cat>
2023-03-03 23:19:12 +0100

    ICO smart contract

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xFA5d724b8c5C652878bB673245de666B38024D9C";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
